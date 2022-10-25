21eb639e620c172abb001764d4262596e660baeb
Frank S <fgunni@01019freenet.de>
2022-10-25 20:41:39 +0200

    baseUri should be set correctly

--- custom/DAVServer/calendarserver.php
+++ custom/DAVServer/calendarserver.php
@@ -27,7 +27,7 @@ $authcontroller = AuthenticationController::getInstance();
 //date_default_timezone_set('Canada/Eastern');
 // If you want to run the SabreDAV server in a custom location (using mod_rewrite for instance)
 // You can override the baseUri here.
-//$baseUri = 'SuiteCRM/custom/DAVServer/calendarserver.php';
+$baseUri = '/custom/DAVServer/calendarserver.php';
 //Mapping PHP errors to exceptions
 function exception_error_handler($errno, $errstr, $errfile, $errline) {
     throw new ErrorException($errstr, 0, $errno, $errfile, $errline);
