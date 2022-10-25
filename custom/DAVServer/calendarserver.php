486cd35fa1bd681d96585831ee4b33f1eb354a1b
Frank Sagurna <sagurna@next-kraftwerke.de>
2022-10-25 21:14:06 +0200

    Prepare to restructure, merge both servers

--- custom/DAVServer/calendarserver.php
+++ custom/DAVServer/calendarserver.php
@@ -11,23 +11,16 @@ require_once 'modules/Users/authentication/AuthenticationController.php';
 
 // Requires maybe not possible if everything is correctly installed with composer
 require_once 'custom/DAVServer/SugarCalDAVBackend.php';
+require_once 'custom/DAVServer/SugarCardDAVBackend.php';
 require_once 'custom/DAVServer/SugarDAVACLPrincipalBackend.php';
 
 $authcontroller = AuthenticationController::getInstance();
-
-/*
-
-  CalendarServer example
-
-  This server features CalDAV support
-
- */
 
 // settings
 //date_default_timezone_set('Canada/Eastern');
 // If you want to run the SabreDAV server in a custom location (using mod_rewrite for instance)
 // You can override the baseUri here.
-$baseUri = '/custom/DAVServer/calendarserver.php';
+$baseUri = '/custom/DAVServer/davserver.php';
 //Mapping PHP errors to exceptions
 function exception_error_handler($errno, $errstr, $errfile, $errline) {
     throw new ErrorException($errstr, 0, $errno, $errfile, $errline);
@@ -36,15 +29,17 @@ function exception_error_handler($errno, $errstr, $errfile, $errline) {
 //set_error_handler("exception_error_handler");
 // Files we need
 //require_once 'SabreDAV/vendor/autoload.php';
-// Sugar Autoloader used 
+// Sugar Autoloader used
 // Backends
 $authBackend = new Sabre\DAV\Auth\Backend\BasicCallBack(Array($authcontroller, "login"));
-$calendarBackend = new Custom\DAVServer\SugarCalDAVBackend($sugar_config);
 $principalBackend = new Custom\DAVServer\SugarDAVACLPrincipalBackend($sugar_config);
+$addressBookBackend = new Custom\DAVServer\SugarCardDAVBackend($sugar_config);
+$calendarBackend = new Custom\DAVServer\SugarCalDAVBackend($sugar_config);
 
 // Directory structure
 $tree = [
     new Sabre\CalDAV\Principal\Collection($principalBackend),
+    new Sabre\CardDAV\AddressBookRoot($principalBackend, $addressBookBackend),
     new Sabre\CalDAV\CalendarRoot($principalBackend, $calendarBackend),
 ];
 
@@ -54,32 +49,26 @@ if (isset($baseUri))
     $server->setBaseUri($baseUri);
 
 /* Server Plugins */
-$authPlugin = new Sabre\DAV\Auth\Plugin($authBackend);
-$server->addPlugin($authPlugin);
+$server->addPlugin(new Sabre\DAV\Auth\Plugin($authBackend));
 
-$aclPlugin = new Sabre\DAVACL\Plugin();
-$server->addPlugin($aclPlugin);
+$server->addPlugin(new Sabre\DAVACL\Plugin());
+
+/* CardDAV support */
+$server->addPlugin(new Sabre\CardDAV\Plugin());
 
 /* CalDAV support */
-$caldavPlugin = new Sabre\CalDAV\Plugin();
-$server->addPlugin($caldavPlugin);
-
+$server->addPlugin(new Sabre\CalDAV\Plugin());
 /* Calendar subscription support */
-$server->addPlugin(
-        new Sabre\CalDAV\Subscriptions\Plugin()
-);
+$server->addPlugin(new Sabre\CalDAV\Subscriptions\Plugin());
 
 /* Calendar scheduling support */
-$server->addPlugin(
-        new Sabre\CalDAV\Schedule\Plugin()
-);
+$server->addPlugin(new Sabre\CalDAV\Schedule\Plugin());
 
 /* WebDAV-Sync plugin */
 $server->addPlugin(new Sabre\DAV\Sync\Plugin());
 
 // Support for html frontend
-$browser = new Sabre\DAV\Browser\Plugin();
-$server->addPlugin($browser);
+$server->addPlugin(new Sabre\DAV\Browser\Plugin());
 
 // And off we go!
 $server->exec();
